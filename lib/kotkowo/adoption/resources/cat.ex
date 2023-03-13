2df817bfa5346bb0312198adf8f53581f6d4018d
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-03-13 19:31:28 +0100

    🙀 Add some cat attrs

--- lib/kotkowo/adoption/resources/cat.ex
+++ lib/kotkowo/adoption/resources/cat.ex
@@ -16,5 +16,31 @@ defmodule Kotkowo.Adoption.Cat do
     attribute :name, :string do
       allow_nil? false
     end
+
+    attribute :sex, :atom do
+      constraints [
+        one_of: [:male, :female]
+      ]
+    end
+
+    attribute :age, :atom do
+      constraints [
+        one_of: [:junior, :senior]
+      ]
+    end
+
+    attribute :fiv_felv_status, :atom do
+      constraints [
+        one_of: [:both_negative, :both_positive]
+      ]
+    end
+
+    attribute :health_status, :atom do
+      constraints [
+        one_of: [:healthy, :sick]
+      ]
+    end
+
+    attribute :castrated, :boolean
   end
 end
