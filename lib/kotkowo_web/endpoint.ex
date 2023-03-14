a70cbe5ed83eeac9162fd5ebf60b043b99ea3f5e
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-03-14 07:09:50 +0100

    🔧 Setup corsica
    TODO: make strict for prod

--- lib/kotkowo_web/endpoint.ex
+++ lib/kotkowo_web/endpoint.ex
@@ -1,6 +1,13 @@
 defmodule KotkowoWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :kotkowo
 
+  plug Corsica,
+    max_age: 600,
+    # [~r{^https?://(.*.?)ravensiris.xyz$}, "http://localhost", "http://127.0.0.1"]
+    origins: "*",
+    log: [rejected: :info, accepted: false],
+    allow_headers: :all
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
